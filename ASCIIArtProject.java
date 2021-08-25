df790c705dd0158491b88022cd49ac51e6681995
Alex Vance <20475@my4county.net>
2021-08-25 10:06:34 -0400

    trying to set  up my ascii art properly

---
+++ ASCIIArtProject.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ascii.art.project;
+
+/**
+ *
+ * @author avance
+ */
+public class ASCIIArtProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("     ___________  _____  ");
+        System.out.println("    /          / /    / ");
+        System.out.println("   /  ________/ /    /  ");
+        System.out.println("  /  /       / /    /        ");//had to shift ir right for some reason
+        System.out.println(" /  / ______/ /    / ");//changed from orginal design, looks more similar to the modern design
+        System.out.println("/__/_/       /____/");
+        System.out.println("       FORMULA ");
+        System.out.println("         ONE");
+    //I keep making changed to the shape of the F
+    }
+     
+    
+    
+    
+    
+}
